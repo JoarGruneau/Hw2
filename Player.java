434ebcac1aec140f27461705a1124489db7702e5
Joar Gruneau <joar@gruneau.se>
2016-09-16 01:30:17 +0200

    tic tac toe very basic minMax search

--- Player.java
+++ Player.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
+
 public class Player {
     /**
-     * Performs a move
+     * Performs a mo
      *
      * @param gameState
      *            the current state of the board
@@ -18,12 +19,87 @@ public class Player {
             // Must play "pass" move if there are no other moves possible.
             return new GameState(gameState, new Move());
         }
+                    
+        if(gameState.getNextPlayer()==Constants.CELL_O){
+            int best_value_move=-1;
+            int tmp_value_move;
+            GameState best_move=nextStates.firstElement();
+            for(GameState tmp_state: nextStates){ 
+                tmp_value_move=searchMinMax(tmp_state, Constants.CELL_O, 0); 
+                    //System.err.println(tmp_state.toString(Constants.CELL_O));
+                    //System.err.println(tmp_value_move);
+                    //System.err.println(end(tmp_state));
+                if( tmp_value_move>best_value_move){
+                    best_value_move=tmp_value_move;
+                    best_move=tmp_state;
 
-        /**
-         * Here you should write your algorithms to get the best next move, i.e.
-         * the best next state. This skeleton returns a random move instead.
-         */
-        Random random = new Random();
-        return nextStates.elementAt(random.nextInt(nextStates.size()));
-    }    
+                }
+            }
+                    
+            return best_move;
+        }
+
+        else{
+            Random random = new Random();
+            GameState move=nextStates.elementAt(random.nextInt(nextStates.size()));
+            return move;
+        }
+    }
+
+    public int searchMinMax(GameState game_state, int player, int depth){
+        Vector<GameState> nextStates = new Vector<>();
+        game_state.findPossibleMoves(nextStates);
+        int best_move;
+
+        if(game_state.isOWin()){
+            return -1;
+        }
+        else if(game_state.isXWin()){
+            return 1;
+        }
+        else if(depth==3){
+            return 0;
+        }
+        else{
+        
+        if(Constants.CELL_X==player){
+            best_move=-1;
+            for(GameState state:nextStates){
+                int move = searchMinMax(state, Constants.CELL_O, depth+1);
+                best_move=Math.max(move, best_move);
+            }
+            return best_move;
+        }
+        else{
+            best_move=1;
+            for(GameState state:nextStates){
+                int move = searchMinMax(state, Constants.CELL_X, depth+1);
+                best_move=Math.min(move, best_move);
+            }
+            return best_move;
+            
+        }
+        }
+
+    }
+    public boolean end(GameState game_state){
+        if(game_state.isOWin()){
+            return true;
+        }
+        else return game_state.isXWin();
+           
+    }
+    
+    
+    public int value(GameState game_state){
+        if(game_state.isXWin()){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+
+
+
+    
 }
