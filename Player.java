0b3e7d1fa8d27ef4ada3876ff1a1a7bbd486e89e
= <felixab@kth.se>
2016-09-19 22:52:18 +0200

    Getting 86 at run depth 1 with 13s runtime, might need repeated state checking or negamax at least

--- Player.java
+++ Player.java
@@ -5,6 +5,8 @@ public class Player {
     int value_loss=-1000;
     int value_win=1000;
     int value_tie=-100;
+    int BOARD_SIZE = 4;
+    int max_depth = 2;
     /**
      * Performs a mo
      *
@@ -30,7 +32,7 @@ public class Player {
             System.err.println("new turn -------------------------------------------------");
             for(GameState tmp_state: nextStates){ 
                 tmp_value_move=searchMinMax(tmp_state, 
-                        Constants.CELL_O, 5, value_loss,value_win);
+                        Constants.CELL_O, max_depth, value_loss,value_win);
                     System.err.println(tmp_value_move);
                     System.err.println(tmp_state.toString(Constants.CELL_O));
                 if( tmp_value_move>=best_value_move){
@@ -78,7 +80,7 @@ public class Player {
         }
         else{
         
-        if(Constants.CELL_X==player){
+        if(player==Constants.CELL_X){
             best_move=value_loss;
             for(GameState state:nextStates){
                 int move = searchMinMax(state,
@@ -127,94 +129,216 @@ public class Player {
         
     }
     public int value(GameState game_state, int player){
+        // Returns value of all 76 rows. In total 76*4 cells are checked, right?
         return valueRows(game_state, player)
-            + valueColumns(game_state, player)
-                +valueDiagonals(game_state, player);
+                + valueColumns(game_state, player)
+                + valueLayers(game_state, player)
+                + valueDiagonals(game_state, player);
     }
     public int valueDiagonals(GameState game_state, int player){
-        return valueDiagonal(game_state, player, 0,-1)+
-                valueDiagonal(game_state, player, 3,1);
+        // Returns the value of the 24 normal diagonals and the 4 main diagonals
+        return valueDiagonalRows(game_state, player)
+            + valueDiagonalColumns(game_state, player)
+            + valueDiagonalLayers(game_state, player)
+            + valueMainDiagonals(game_state, player);
     }
-    public int valueDiagonal(GameState game_state, int player,
-            int start_row, int up_or_down){
-        // 1 for go up and -1 for go down
-        int tmp_value_diagonal=0;
-        int col=0;
-        int cell=game_state.at(start_row,col);
-        while(cell!=Constants.CELL_INVALID){
-            if(cell==player){
-                tmp_value_diagonal++;
-            }
-            else if(cell== Constants.CELL_EMPTY){}  // Why not include this in the next else?
-            else{
-                tmp_value_diagonal=0;
-                break;
-            }
-            col++;
-            start_row+=up_or_down;
-            cell=game_state.at(start_row,col);
-        }
-        return tmp_value_diagonal*tmp_value_diagonal; // Maybe generalize to Math.pow(tmp_value_diagonal, some_factor).
-    }
-    public int valueRows(GameState game_state, int player){
+    public int valueMainDiagonals(GameState game_state, int player){
+        // Returns the value of the 4 main diagonals
+        int tmp_value = 0;
+        int tmp_value_diagonal;
+        int cell;
+        int row;
         int col;
-        int row=0;
-        int tmp_value=0;
-        int tmp_value_row;
-        int cell=game_state.at(0, 0);
-        while(cell!= Constants.CELL_INVALID){
-            tmp_value_row=0;
-            col=0;
-            while(cell!=Constants.CELL_INVALID){
-                if(cell==player){
-                    tmp_value_row++;
+        for(int direction_row = -1; direction_row < 3; direction_row += 2){
+            for(int direction_col = -1; direction_col < 3; direction_col += 2){
+                // row and col start from top or bottom
+                row = (direction_row == -1) ? BOARD_SIZE-1 : 0;
+                col = (direction_col == -1) ? BOARD_SIZE-1 : 0;
+                tmp_value_diagonal = 0;
+                for(int layer = 0; layer<BOARD_SIZE; layer++){
+                    cell = game_state.at(row,col,layer);
+                    if(cell==player){
+                        tmp_value_diagonal++;
+                    }
+                    else if(cell== Constants.CELL_EMPTY){}  // Why not include this in the next else?
+                    else{
+                        tmp_value_diagonal=0;
+                        break;
+                    }
+                    row += direction_row;
+                    col += direction_col;
                 }
-                else if(cell==Constants.CELL_EMPTY){}
-                else{
-                    tmp_value_row=0;
-                    break; 
-                }
-                col++;
-                cell=game_state.at(row, col); // weird to update cell in final step of loop, but w/e
-                
+                tmp_value += tmp_value_diagonal;
             }
-            tmp_value+=tmp_value_row*tmp_value_row; // Generalize with Math.pow
-            row++;
-            cell=game_state.at(row, 0);
+        }    
+        return tmp_value;
+    }
+    public int valueDiagonalRows(GameState game_state, int player){
+        // Returns value of the 8 normal diagonals over the rows
+        int tmp_value = 0;
+        int tmp_value_diagonal;
+        int cell;
+        int layer;
+        for(int row=0; row<BOARD_SIZE; row++){
+            for(int direction = -1; direction<3; direction += 2){ 
+                // 'direction' determines direction of diagonal, up or down
+                tmp_value_diagonal = 0;
+                // Layer starts from top or bottom
+                layer = (direction == -1) ? BOARD_SIZE-1 : 0;
+                for(int col=0; col<BOARD_SIZE; col++){
+                    cell = game_state.at(row,col,layer);
+                    if(cell==player){
+                        tmp_value_diagonal++;
+                    }
+                    else if(cell== Constants.CELL_EMPTY){}  // Why not include this in the next else?
+                    else{
+                        tmp_value_diagonal=0;
+                        break;
+                    }
+                    layer += direction;
+                }
+                tmp_value += tmp_value_diagonal*tmp_value_diagonal;
+            }
+
         }
         return tmp_value;
     }
+    public int valueDiagonalColumns(GameState game_state, int player){
+        // Returns value of the 8 normal diagonals over the columns
+        int tmp_value = 0;
+        int tmp_value_diagonal;
+        int cell;
+        int layer;
+        for(int col=0; col<BOARD_SIZE; col++){
+            for(int direction = -1; direction<3; direction += 2){ 
+                // 'direction' determines direction of diagonal, up or down
+                tmp_value_diagonal = 0;
+                // Layer starts from top or bottom
+                layer = (direction == -1) ? BOARD_SIZE-1: 0;
+                for(int row=0; row<BOARD_SIZE; row++){
+                    cell = game_state.at(row,col,layer);
+                    if(cell==player){
+                        tmp_value_diagonal++;
+                    }
+                    else if(cell== Constants.CELL_EMPTY){}  // Why not include this in the next else?
+                    else{
+                        tmp_value_diagonal=0;
+                        break;
+                    }
+                    layer += direction;
+                }
+                tmp_value += tmp_value_diagonal*tmp_value_diagonal;
+            }
+
+        }
+        return tmp_value;
+    }
+    public int valueDiagonalLayers(GameState game_state, int player){
+        // Returns value of the 8 normal diagonals over the layers
+        int tmp_value = 0;
+        int tmp_value_diagonal;
+        int cell;
+        int col;
+        for(int layer=0; layer<BOARD_SIZE; layer++){
+            for(int direction = -1; direction<3; direction += 2){ 
+                // 'direction' determines direction of diagonal, up or down
+                tmp_value_diagonal = 0;
+                // Column starts from top or bottom
+                col = (direction == -1) ? BOARD_SIZE-1 : 0;
+                for(int row=0; row<BOARD_SIZE; row++){
+                    cell = game_state.at(row,col,layer);
+                    if(cell==player){
+                        tmp_value_diagonal++;
+                    }
+                    else if(cell== Constants.CELL_EMPTY){}  // Why not include this in the next else?
+                    else{
+                        tmp_value_diagonal=0;
+                        break;
+                    }
+                    col += direction;
+                }
+                tmp_value += tmp_value_diagonal*tmp_value_diagonal;
+            }
+
+        }
+        return tmp_value;
+    }
+
+    public int valueRows(GameState game_state, int player){
+        // Rewritten for 3D
+        // Returns value of 16 rows
+        int tmp_value = 0;
+        int tmp_value_row;
+        int cell;
+        for(int layer=0; layer<BOARD_SIZE; layer++){
+            for(int row=0; row<BOARD_SIZE; row++){
+                tmp_value_row=0;
+                for(int col=0; col<BOARD_SIZE; col++){
+                    cell = game_state.at(row,col,layer);
+                    if(cell == player){
+                        tmp_value_row++;
+                    }
+                    else if(cell==Constants.CELL_EMPTY){} // why not include this in next else?
+                    else{
+                        tmp_value_row=0;
+                        break; 
+                    }
+                }
+                tmp_value+=tmp_value_row*tmp_value_row; // Generalize with Math.pow
+            }
+        }
+        return tmp_value;
+    }
+
     public int valueColumns(GameState game_state, int player){
-        int col=0;
-        int row;
+        //Rewritten for 3D
+        // Returns value of 16 rows
         int tmp_value=0;
         int tmp_value_column;
-        int cell=game_state.at(0, 0);
-        while(cell!= Constants.CELL_INVALID){
-            tmp_value_column=0;
-            row=0;
-            while(cell!=Constants.CELL_INVALID){
-                if(cell==player){
-                    tmp_value_column++;
+        int cell;
+        for(int layer=0; layer<BOARD_SIZE; layer++){
+            for(int col=0; col<BOARD_SIZE; col++){
+                tmp_value_column=0;
+                for(int row=0; row<BOARD_SIZE; row++){
+                    cell=game_state.at(row,col,layer);
+                    if(cell==player){
+                        tmp_value_column++;
+                    }
+                    else if(cell==Constants.CELL_EMPTY){} // why not include this in next else?
+                    else{
+                        tmp_value_column=0;
+                        break;
+                    }
+                    
                 }
-                else if(cell==Constants.CELL_EMPTY){
-                }
-                else{
-                    tmp_value_column=0;
-                    break;
-                }
-                row++;
-                cell=game_state.at(row, col);
-                
+                tmp_value+=tmp_value_column*tmp_value_column; // generalize with math.pow
             }
-            tmp_value+=tmp_value_column*tmp_value_column; // generalize with math.pow
-            col++;
-            cell=game_state.at(0, col);
         }
         return tmp_value;
     }
 
-
-
-    
+    public int valueLayers(GameState game_state, int player){
+        // Returns value of 16 rows
+        int tmp_value=0;
+        int tmp_value_layer_part;
+        int cell;
+        for(int row=0; row<BOARD_SIZE; row++){
+            for(int col=0; col<BOARD_SIZE; col++){
+                tmp_value_layer_part = 0;
+                for(int layer=0; layer<BOARD_SIZE; layer++){
+                    cell=game_state.at(row,col,layer);
+                    if(cell==player){
+                        tmp_value_layer_part++;
+                    }
+                    else if(cell==Constants.CELL_EMPTY){} // why not include this in next else?
+                    else{
+                        tmp_value_layer_part=0;
+                        break;
+                    }
+                }
+                tmp_value+=tmp_value_layer_part*tmp_value_layer_part; // generalize with math.pow
+            }
+        }
+        return tmp_value;
+    }
 }
